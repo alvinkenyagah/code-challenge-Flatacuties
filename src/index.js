33a416ccd51818681ab965b49a79117c45619f83
alvinkenyagah <alvin47k@gmail.com>
2023-03-25 19:41:25 +0300

    Added return logic for number of votes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-// Your code here
+document.addEventListener("DOMContentLoaded", () => {
+  addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let votes = document.querySelector("#votes").value;
+    let votecount = document.querySelector("#vote-count");
+
+    if (isNaN(votes)) {
+      votes = 0;
+    } 
+
+    // additionalVotes+=votes;
+
+
+       votecount.innerHTML = votes;
+
+       const reset = document.querySelector("#reset-btn");
+       reset.addEventListener("click", () => {
+         votecount.innerHTML = "0";
+       });
+
+
+  });
+
+
+});
+
+
+
+
